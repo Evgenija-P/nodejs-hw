eb07e0ae46cfe95760e7bf8c0015308524d49855
Evgenija-P <e.a.poduzova@gmail.com>
2023-01-10 23:25:01 +0200

    Jimp

--- controllers/user/updateAvatar.js
+++ controllers/user/updateAvatar.js
@@ -1,3 +1,4 @@
+const Jimp = require("jimp");
 const fs = require("fs/promises");
 const path = require("path");
 
@@ -8,15 +9,18 @@ const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 const updateAvatar = async (req, res) => {
   try {
     const { path: tempUpload, filename } = req.file;
+    const tempPath = req.file.path;
+
     const { _id } = req.user;
     const [extention] = filename.split(".").reverse();
-    const avatarName = `${_id}.${extention}`;
 
-    const resultUpload = path.join(avatarsDir, avatarName);
+    const image = await Jimp.read(tempPath);
+    image.resize(250, 250);
+    image.write(`${avatarsDir}/${_id}.${extention}`);
 
-    await fs.rename(tempUpload, resultUpload);
+    fs.unlink(tempUpload);
 
-    const avatarUrl = path.join("avatars", resultUpload);
+    const avatarUrl = `${avatarsDir}/${_id}.${extention}`;
 
     await User.findByIdAndUpdate(_id, { avatarUrl });
 
@@ -34,3 +38,33 @@ const updateAvatar = async (req, res) => {
 };
 
 module.exports = updateAvatar;
+
+// try {
+//     const { path: tempUpload, filename } = req.file;
+
+//     const { _id } = req.user;
+//     const [extention] = filename.split(".").reverse();
+//     const avatarName = `${_id}.${extention}`;
+//     console.log("avatarsDir", avatarsDir);
+//     console.log("avatarName", avatarName);
+//     const resultUpload = path.join(avatarsDir, avatarName);
+//     console.log("resultUpload", resultUpload);
+//     const image = await Jimp.read(resultUpload);
+//     image.resize(250, 250);
+//     image.write(`${avatarsDir}/${_id}1.${extention}`);
+
+//     await fs.rename(tempUpload, resultUpload);
+
+//     const avatarUrl = path.join("avatars", resultUpload);
+//     console.log("avatarUrl", avatarUrl);
+
+//     await User.findByIdAndUpdate(_id, { avatarUrl });
+
+//     res.json({
+//       status: "success",
+//       code: 200,
+//       data: {
+//         avatarUrl,
+//       },
+//     });
+//   }
